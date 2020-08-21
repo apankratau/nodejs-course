07596c95250e9b9222a7ee10bddd27dcdb01ea5a
gldstn <beetok1996@gmail.com>
2020-08-21 10:48:27 +0300

    Implement requested changes

--- module-1/task-3/task-1/index.js
+++ module-1/task-3/task-1/index.js
@@ -1,19 +1,15 @@
 import readline from 'readline';
 
-const processLineByLine = () => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: true,
-  });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true,
+});
 
-  rl.on('line', (line) => {
-    const reversedLine = line.split('').reverse().join('');
-    readline.moveCursor(process.stdout, 0, -1);
-    console.log(reversedLine + '\n');
-  }).on('close', () => {
-    process.exit(0);
-  });
-};
-
-processLineByLine();
+rl.on('line', (line) => {
+  const reversedLine = line.split('').reverse().join('');
+  readline.moveCursor(process.stdout, 0, -1);
+  console.log(reversedLine + '\n');
+}).on('close', () => {
+  process.exit(0);
+});
